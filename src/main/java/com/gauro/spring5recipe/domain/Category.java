4af49e6d8f338debc736f7d9f87ce8c2bb4ec59c
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-16 05:07:31 -0500

    Spring8 Lect 143 Recipes App added BootStrap

--- src/main/java/com/gauro/spring5recipe/domain/Category.java
+++ src/main/java/com/gauro/spring5recipe/domain/Category.java
@@ -1,14 +1,18 @@
 package com.gauro.spring5recipe.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.util.Set;
 
 /**
  * @author Chandra
  */
+@Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,36 +22,36 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", description='" + description + '\'' +
-                ", recipes=" + recipes +
-                '}';
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public Set<Recipe> getRecipes() {
+//        return recipes;
+//    }
+//
+//    public void setRecipes(Set<Recipe> recipes) {
+//        this.recipes = recipes;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Category{" +
+//                "id=" + id +
+//                ", description='" + description + '\'' +
+//                ", recipes=" + recipes +
+//                '}';
+//    }
 }
