cd5ffa23ce3e87585e4b4db453f760e7a182e339
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-23 12:15:58 -0500

    Spring 10 Lect 198  Recipes App added  CRUD REstController and Spring command converter and Test Case

--- src/main/java/com/gauro/spring5recipe/controllers/RecipeController.java
+++ src/main/java/com/gauro/spring5recipe/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.gauro.spring5recipe.controllers;
 
+import com.gauro.spring5recipe.commands.RecipeCommand;
 import com.gauro.spring5recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -20,9 +23,23 @@ public class RecipeController {
     }
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
+        log.debug("showById is called ======>"+id);
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         log.info(recipeService.findById(Long.valueOf(id)).toString());
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        log.debug("newRecipe is called ======>");
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand saveCommand=recipeService.saveRecipeCommand(command);
+        log.debug("redirect:/recipe/show/  is calling======>"+saveCommand.getId());
+        return "redirect:/recipe/show/"+saveCommand.getId();
     }
 }
